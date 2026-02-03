97f4ff50b4d0ccde08d15aff8a1e531709266f2f
Jeremie Bela <Jeremiebela@hotmail.com>
2026-02-03 12:56:18 +0200

    Remove old non-auth app and fix routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { AuthProvider } from "./context/AuthContext";
+import Dashboard from "./pages/Dashboard";
+import Auth from "./pages/Auth";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
+function Root() {
+  const { token } = useContext(AuthContext);
+  return token ? <Dashboard /> : <Auth />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <Root />
+    </AuthProvider>
   </React.StrictMode>
 );
+
