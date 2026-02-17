4a7fbade35e00347bfc88bf9371e7b163e070fbf
Jeremie Bela <Jeremiebela@hotmail.com>
2026-02-17 21:05:00 +0200

    fix api.js syntax

--- src/api.js
+++ src/api.js
@@ -1,20 +1,20 @@
 const BASE_URL = "https://skillsbet-production-37ae.up.railway.app";
 
 export const api = {
-  async login(email, password) {
+  async login(username, password) {
     const res = await fetch(`${BASE_URL}/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ username, password }),
     });
     return res.json();
   },
 
-  async register(email, password) {
+  async register(username, password) {
     const res = await fetch(`${BASE_URL}/register`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ username, password }),
     });
     return res.json();
   },
@@ -25,33 +25,30 @@ export const api = {
   },
 
   async addSkill(token, name, level, category) {
-  const res = await fetch(`${BASE_URL}/skills`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-      name,
-      level,
-      category
-    }),
-  });
+    const res = await fetch(`${BASE_URL}/skills`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name,
+        level,
+        category
+      }),
+    });
 
-  if (!res.ok) {
-    const text = await res.text();
-    throw new Error(text);
-  }
+    if (!res.ok) {
+      const text = await res.text();
+      throw new Error(text);
+    }
 
-  return res.json();
-}
+    return res.json();
+  },
 
   async getSkills() {
     const res = await fetch(`${BASE_URL}/skills`);
     return res.json();
-  },
+  }
 };
-
-
-
 
